6e798b4d3cdfc0b459a5ea63c779821f9dd679d3
ani1311 <aniket.alur13111997@gmail.com>
2023-10-23 19:39:12 -0700

    Fix: Fixed lint

--- app.go
+++ app.go
@@ -90,7 +90,7 @@ var slackRetryErrors = map[string]string{
 
 var doNotProcessChannels = map[string]time.Time{}
 
-func CheckError(err string, channel string) (retryable bool, pause bool, description string) {
+func CheckError(err string) (retryable bool, pause bool, description string) {
 	// Special case for channel_not_found, we don't want to retry this one right away.
 	// We are making it a 'soft failure' so that we don't keep retrying it for a period of time for any message that is sent to a channel that doesn't exist.
 	if err == "channel_not_found" {
@@ -207,7 +207,7 @@ func (app *App) processQueue(ctx context.Context, maxRetries int, initialBackoff
 				err := app.messenger.PostMessage(msg, slackPostMessageURL, tokenFlag)
 				//nolint:nestif // but simplify by not having else at least.
 				if err != nil {
-					retryable, pause, description := CheckError(err.Error(), msg.Channel)
+					retryable, pause, description := CheckError(err.Error())
 
 					// We keep track of channels that are paused in a map, and we will retry it after a period of time.
 					if pause {
